5b5d2ed17dffd20a4cb083f839ec8a4360efe8a7
Sanruethai <sanruethai.s@kkumail.com>
2023-04-07 02:37:50 +0700

    sanruethai Saybut

---
+++ gr/gr/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace gr
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label6 = new Label();
+            textBox6 = new TextBox();
+            label10 = new Label();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            textBox4 = new TextBox();
+            textBox2 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            groupBox1 = new GroupBox();
+            tbListofPerson = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            tbTotal = new TextBox();
+            label5 = new Label();
+            btnAdd = new Button();
+            tbBirthYear = new TextBox();
+            label2 = new Label();
+            tbName = new TextBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(19, 36);
+            label6.Name = "label6";
+            label6.Size = new Size(69, 20);
+            label6.TabIndex = 11;
+            label6.Text = "GPA Max";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(270, 69);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(114, 27);
+            textBox6.TabIndex = 20;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(219, 74);
+            label10.Name = "label10";
+            label10.Size = new Size(26, 20);
+            label10.TabIndex = 19;
+            label10.Text = "ชื่อ";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(270, 31);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(114, 27);
+            textBox5.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(219, 36);
+            label9.Name = "label9";
+            label9.Size = new Size(26, 20);
+            label9.TabIndex = 17;
+            label9.Text = "ชื่อ";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(89, 108);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(114, 27);
+            textBox4.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(89, 31);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(114, 27);
+            textBox2.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(19, 113);
+            label8.Name = "label8";
+            label8.Size = new Size(67, 20);
+            label8.TabIndex = 15;
+            label8.Text = "GPA Avg";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(19, 74);
+            label7.Name = "label7";
+            label7.Size = new Size(66, 20);
+            label7.TabIndex = 13;
+            label7.Text = "GPA Min";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(89, 69);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(114, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(tbListofPerson);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(tbTotal);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Location = new Point(274, 69);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(401, 283);
+            groupBox1.TabIndex = 29;
+            groupBox1.TabStop = false;
+            groupBox1.Text = " ข้อมูลรายวิชา";
+            // 
+            // tbListofPerson
+            // 
+            tbListofPerson.Location = new Point(89, 189);
+            tbListofPerson.Margin = new Padding(3, 4, 3, 4);
+            tbListofPerson.Multiline = true;
+            tbListofPerson.Name = "tbListofPerson";
+            tbListofPerson.Size = new Size(114, 73);
+            tbListofPerson.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 190);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 20);
+            label3.TabIndex = 6;
+            label3.Text = "รายชื่อ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(53, 20);
+            label4.TabIndex = 7;
+            label4.Text = "อายุรวม";
+            // 
+            // tbTotal
+            // 
+            tbTotal.Location = new Point(89, 147);
+            tbTotal.Margin = new Padding(3, 4, 3, 4);
+            tbTotal.Name = "tbTotal";
+            tbTotal.Size = new Size(114, 27);
+            tbTotal.TabIndex = 8;
+            tbTotal.Text = "0";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(89, 181);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 20);
+            label5.TabIndex = 27;
+            label5.Text = "GPA";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(140, 216);
+            btnAdd.Margin = new Padding(3, 4, 3, 4);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(114, 64);
+            btnAdd.TabIndex = 26;
+            btnAdd.Text = "Add";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // tbBirthYear
+            // 
+            tbBirthYear.Location = new Point(140, 138);
+            tbBirthYear.Margin = new Padding(3, 4, 3, 4);
+            tbBirthYear.Name = "tbBirthYear";
+            tbBirthYear.Size = new Size(114, 27);
+            tbBirthYear.TabIndex = 25;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(89, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 20);
+            label2.TabIndex = 24;
+            label2.Text = "ปีเกิด";
+            // 
+            // tbName
+            // 
+            tbName.Location = new Point(140, 100);
+            tbName.Margin = new Padding(3, 4, 3, 4);
+            tbName.Name = "tbName";
+            tbName.Size = new Size(114, 27);
+            tbName.TabIndex = 23;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(89, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(26, 20);
+            label1.TabIndex = 22;
+            label1.Text = "ชื่อ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(140, 177);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(114, 27);
+            textBox1.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Controls.Add(label5);
+            Controls.Add(btnAdd);
+            Controls.Add(tbBirthYear);
+            Controls.Add(label2);
+            Controls.Add(tbName);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label6;
+        private TextBox textBox6;
+        private Label label10;
+        private TextBox textBox5;
+        private Label label9;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private Label label8;
+        private Label label7;
+        private TextBox textBox3;
+        private GroupBox groupBox1;
+        private TextBox tbListofPerson;
+        private Label label3;
+        private Label label4;
+        private TextBox tbTotal;
+        private Label label5;
+        private Button btnAdd;
+        private TextBox tbBirthYear;
+        private Label label2;
+        private TextBox tbName;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
